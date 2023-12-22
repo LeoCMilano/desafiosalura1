5374f932f5661d8c82f9d7d728a2f8317499315d
Leo Milano <lmilano@outlook.com>
2023-12-21 22:38:15 -0300

    Fim de curso

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,15 +1,19 @@
 function adicionar(){
     let nome = document.getElementById('nome-amigo').value;
-    
+    if(nome==""){
+        alert ('Insira um nome')
+        return;
+    }
     let campoNomes = document.getElementById('lista-amigos');
-    
     if(campoNomes.innerText==''){
         campoNomes.innerText = nome;
+        document.getElementById('nome-amigo').value = '';
+    }else if(campoNomes.innerText.includes(nome)){
+        alert (`O nome ${nome} já está na lista, favor incluir um sobrenome`)
     }else{
         campoNomes.innerText = `${campoNomes.innerText}, ${nome}`;
+        document.getElementById('nome-amigo').value = '';
     }
-    
-    document.getElementById('nome-amigo').value = '';
 }
 
 function sortear2(){
@@ -46,18 +50,22 @@ function reiniciar(){
 function sortear(){
     parts= document.getElementById('lista-amigos').innerText;
     const nome = parts.split(', ');
-    embaralha(nome)
-    let lista = ""
-    i=0
+    if(nome.length<4){
+        alert ("Adicione pelo menos 4 participantes");
+        return;
+    }
+    embaralha(nome);
+    let lista = "";
+    i=0;
     while(i< nome.length){
         if(i==nome.length-1){
-            lista+=nome[i]+'->'+nome[0]
+            lista+=nome[i]+'->'+nome[0];
         }else{
-            lista+=nome[i]+'->'+nome[i+1]+'<br>'
+            lista+=nome[i]+'->'+nome[i+1]+'<br>';
         }
-    i++
+    i++;
     }
-    document.getElementById('lista-sorteio').innerHTML = lista
+    document.getElementById('lista-sorteio').innerHTML = lista;
 }
 
 
